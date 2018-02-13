512e505eff91c14e48fa558d64d450db944533e6
SamyStyle <sven.mayer@vis.uni-stuttgart.de>
2018-02-13 17:45:47 +0100

    ordered maps by size

--- routes/games.js
+++ routes/games.js
@@ -5,11 +5,28 @@ var GAME_CONFIG = require('../config.json');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    var maps = [];
+    var mapsDic = [];
     glob("public/maps/*.js", function (er, files) {
         for (var i = 0; i<files.length; i++){
-            maps.push(files[i].split(".")[0].split("/")[files[i].split("/").length-1].replace("map_", "").replace("_", "/"));
+			var mapName = files[i].split(".")[0].split("/")[files[i].split("/").length-1].replace("map_", "").replace("_", "/");
+			var sizeOfMap = mapName.split("/");
+            mapsDic.push({
+				name: mapName,
+				value: sizeOfMap[0] * sizeOfMap[1]
+			});
         }
+		
+		var mapsSort = mapsDic.sort(function(a, b) {
+		  return (a.value > b.value) ? 1 : ((b.value > a.value) ? -1 : 0)
+		});
+		
+		var maps = [];
+		for (var i = 0; i<mapsSort.length; i++){
+			maps.push(mapsSort[i].name);
+		}
+		
+
+		
         res.render('games', {mapoptions : maps, gamemodes: GAME_CONFIG.GAME_MODES, port : GAME_CONFIG.CONFIG_PORT});
     });
 });
